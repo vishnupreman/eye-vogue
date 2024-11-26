cd84ed6f745e23b20ed85efbdaa55411d34aa57d
vishnupreman <vishnupreman1011@gmail.com>
2024-11-26 09:40:55 +0530

    google auth change

--- config/googleauth.js
+++ config/googleauth.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const userModel = require('../model/userModel') // Import your user model
 require('dotenv').config()
+const walletModel = require('../model/wallet')
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
@@ -10,8 +11,10 @@ passport.use(new GoogleStrategy({
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
-            // Find or create user in your database
+            // Find the user by Google ID
             let user = await userModel.findOne({ googleId: profile.id });
+
+            // If the user doesn't exist, create a new one
             if (!user) {
                 user = new userModel({
                     googleId: profile.id,
@@ -20,9 +23,32 @@ passport.use(new GoogleStrategy({
                     number: null
                 });
                 await user.save();
+
+                // Create a wallet for the new user
+                const wallet = new walletModel({
+                    user: user._id,
+                    balance: 0,
+                    transactions: []
+                });
+                await wallet.save();
+            } else {
+                // Ensure the user has a wallet
+                let wallet = await walletModel.findOne({ user: user._id });
+                if (!wallet) {
+                    wallet = new walletModel({
+                        user: user._id,
+                        balance: 0,
+                        transactions: []
+                    });
+                    await wallet.save();
+                }
             }
+
+            // Continue the authentication process
             return done(null, user);
         } catch (error) {
+            console.error("Error in GoogleStrategy:", error);
             return done(error, false);
         }
     }));
+
